463d41149ae87dfc3f0394015a18bc8f7eb861ca
Prasannakolapkar <kolapkarprasanna@gmail.com>
2026-07-02 14:05:07 +0530

    These are changes

---
+++ Basic Python/demo1.py
@@ -0,0 +1,44 @@
+# print("Hello World!!!")
+
+# name = "Prasanna"
+# Address = "Sangamner"
+# age = 20
+
+# print("My name is ", name)
+# print("I live in ", Address)
+# print("I am ",age,"year old")
+
+# a = "I am a AIML Student..."
+
+# print(a.upper())
+# print(a.lower())
+
+# name = input("Enter Your Name: ")
+# print("Your name is : ",name )
+
+# if len(name) > 5:
+#     print("Name is too long...")
+
+# else:
+#     print("Your name is short...")
+
+# roll = int(input("Enter Your Roll Number: "))
+
+# if roll.is_integer():
+#     print("It is a number ...")
+
+names = ["Prasanna" , "Rahul" , "Sunny" , "Anish"]
+print(names)
+
+# names.append("Vedant")
+# print(names)
+
+# names.pop()
+
+# for i in names:
+#     print(i)
+
+# names.reverse()
+# print(names)
+
+print(names.count("Prasanna"))
---
+++ Basic Python/q1.py
@@ -0,0 +1,14 @@
+prod1 = 300
+prod2 = 200
+prod3 = 500
+prod4 = 750
+prod5 = 100
+
+total = prod1 + prod2 + prod3 + prod4 + prod5
+print("Total is : ",total)
+
+gst = (18/100)*total
+print("GST is : ", gst)
+
+up_total = total + gst
+print("Updated total: ", up_total)
